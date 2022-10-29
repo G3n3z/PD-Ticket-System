46990db9b60a11ad2d75d4fdee676a38722c9bcc
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-10-29 10:57:26 +0100

    Database folder

--- PD-Ticket-System/src/main/java/com/isec/pd22/Main.java
+++ PD-Ticket-System/src/main/java/com/isec/pd22/Main.java
@@ -1,7 +1,13 @@
 package com.isec.pd22;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello world!");
+        Connection  connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/databases/BaseDB/PD-2022-23-TP.db");
+
     }
 }
