7079f9adc8ce660a0264e6785bab4d0ef041804e
jorhug <hugomsjorge@gmail.com>
2022-11-02 14:17:15 +0000

    change on UNNAVAILABLE case change on UPDATING case change on handleClientRequest to match new communication with client

--- src/main/java/com/isec/pd22/server/threads/AttendClientThread.java
+++ src/main/java/com/isec/pd22/server/threads/AttendClientThread.java
@@ -1,18 +1,21 @@
 package com.isec.pd22.server.threads;
 
+import com.isec.pd22.enums.Status;
 import com.isec.pd22.payload.ClientMSG;
 import com.isec.pd22.server.models.InternalInfo;
+import com.isec.pd22.utils.DBCommunicationManager;
 
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
+import java.sql.SQLException;
 
 public class AttendClientThread extends Thread{
     private Socket clientSocket;
     private InternalInfo internalInfo;
+    private DBCommunicationManager dbComm;
 
     public AttendClientThread(Socket clientSocket, InternalInfo internalInfo) {
         this.clientSocket = clientSocket;
@@ -32,18 +35,25 @@ public class AttendClientThread extends Thread{
                 switch (internalInfo.getStatus())
                 {
                     case AVAILABLE -> {handleClientRequest(clientMsg, oos);}
-                    case UPDATING -> {}//TODO
+                    case UPDATING -> {internalInfo.wait();}//TODO
                     case UNAVAILABLE -> {
-                        closeClient(oos);
+                        closeClient();
                         keepGoing = false;}
                 }
 
             } catch (EOFException e){
-                System.out.println("[AttendClientThread] - client input stream was closed: "+ e.getMessage());
+                synchronized (internalInfo) {
+                    if (internalInfo.getStatus() == Status.UNAVAILABLE) {
+                        System.out.println("[AttendClientThread] - server closed client connection: " + e.getMessage());
+                        closeClient();
+                    }
+                }
                 keepGoing = false;
             } catch (IOException | ClassNotFoundException e) {
                 //TODO tratar excecoes lançadas
                 System.out.println("[AttendClientThread] - failed comunication with client: "+ e.getMessage());
+            } catch (InterruptedException e) {
+                System.out.println("[AttendClientThread] - server finished updating: " + e.getMessage());
             }
         }
         //TODO enviar ultima msg ao cliente e fechar
@@ -54,36 +64,52 @@ public class AttendClientThread extends Thread{
         }
     }
 
-    private void handleClientRequest(ClientMSG msgClient, ObjectOutputStream oos) throws IOException {
-        Scanner sc = new Scanner(msgClient.getCommand());
-        String cmd = sc.nextLine();
-        String[] cmdParts = cmd.split(",");
-        if (cmd.startsWith("select")){
-            //TODO
-        }else if (cmd.startsWith("find")) {
-            //TODO
-        }else if (cmd.startsWith("insert")){
-            //TODO
-        }else if (cmd.startsWith("update")){
-            //TODO
-        }else if (cmd.startsWith("delete")){
-            //TODO
-        }else {
-            System.out.println("[AttendClientThread] Unknown Command");
-            return;
+    private void handleClientRequest(ClientMSG msgClient, ObjectOutputStream oos){
+        try {
+            dbComm = new DBCommunicationManager(msgClient, internalInfo);
+            switch (msgClient.getAction()) {
+                case REGISTER_USER -> { dbComm.registerUser(msgClient, oos); }
+                case LOGIN -> { dbComm.loginUser(msgClient, oos); }
+                case EDIT_USER -> {
+                }
+                case CONSULT_RESERVATION -> {
+                }
+                case CONSULT_SPECTACLE -> {
+                }
+                case CHOOSE_SPECTACLE -> {
+                }
+                case VIEW_SEATS -> {
+                }
+                case SUBMIT_RESERVATION -> {
+                }
+                case CANCEL_RESERVATION -> {
+                }
+                case ADD_SPECTACLE -> {
+                }
+                case LOGOUT -> {
+                }
+                case SHUTDOWN -> {
+                }
+
+            }
+            dbComm.close();
+        } catch (SQLException e) {
+            System.out.println("[AttendClientThread] - failed to initialize DB communication: "+ e.getMessage());
         }
 
-        //TODO preencher mensagem de retorno com resultado da query
-        String msgRetorno = new String();
-        ClientMSG ansmsg = new ClientMSG(msgRetorno);
-        oos.writeObject(ansmsg);
+        //TODO preencher mensagem de retorno com resultado da query e enviar a cliente
+
     }
 
-    private void closeClient(ObjectOutputStream oos) throws IOException {
-        ClientMSG ansMsg = new ClientMSG("shutdown");
-        ansMsg.setServerList(internalInfo.getHeatBeats());
-        oos.writeObject(ansMsg);
-        clientSocket.close();
+    private void closeClient() {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+            ClientMSG ansMsg = new ClientMSG();
+            ansMsg.setServerList(internalInfo.getHeatBeats());
+            oos.writeObject(ansMsg);
+        } catch (IOException e) {
+            System.out.println("[AttendClientThread] - failed to send server list" + e.getMessage());
+        }
     }
 
 }
