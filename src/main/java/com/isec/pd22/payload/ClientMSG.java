38ea69367e99dd188d2277e304b795bc10e3a626
jorhug <hugomsjorge@gmail.com>
2022-11-02 14:19:15 +0000

    update on ClientMSG to match new way to communicate with client

--- src/main/java/com/isec/pd22/payload/ClientMSG.java
+++ src/main/java/com/isec/pd22/payload/ClientMSG.java
@@ -1,31 +1,64 @@
 package com.isec.pd22.payload;
 
-import com.isec.pd22.server.models.ServerHeartBeat;
+import com.isec.pd22.enums.ClientActions;
+import com.isec.pd22.enums.Role;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ClientMSG {
-    private String command; //string de onde virá a query do cliente e onde seguirá a resposta do servidor
+public class ClientMSG implements Serializable {
+    ClientActions action;
+    Role role;
+    String username;
+    String nome;
+    String password;
+    Spectacle spectacle;
+
     private Set<HeartBeat> serverList;
 
-    public ClientMSG() {}
-
-    public ClientMSG(String command) {
-        this.command = command;
+    public ClientMSG() {
         this.serverList = new HashSet<>();
     }
 
-    public String getCommand() {
-        return command;
+    public ClientActions getAction() {
+        return action;
     }
 
-    public void setCommand(String command) {
-        this.command = command;
+    public void setAction(ClientActions action) {
+        this.action = action;
     }
 
-    public Set<HeartBeat> getServerList() {
-        return serverList;
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setServerList(Set<HeartBeat> serverList) {
