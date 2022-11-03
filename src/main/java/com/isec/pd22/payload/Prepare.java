8991ad3a9fbc80f35bc5b47fb2fd4075388ac3ba
jorhug <102927982+jorhug@users.noreply.github.com>
2022-11-03 16:06:57 +0000

    updated prepare

--- src/main/java/com/isec/pd22/payload/Prepare.java
+++ src/main/java/com/isec/pd22/payload/Prepare.java
@@ -1,10 +1,26 @@
 package com.isec.pd22.payload;
 
+import com.isec.pd22.enums.TypeOfMulticastMsg;
 import com.isec.pd22.server.models.Query;
+
+import java.net.Socket;
 
 public class Prepare extends MulticastMSG{
     Query query;
     int numVersion;
+    int confirmationUDPPort;
+    String ip;
+    int portTcpClients;
+
+    public Prepare() {
+    }
+
+    public Prepare(TypeOfMulticastMsg typeMsg, Query query, int numVersion, int confirmationUDPPort) {
+        super(typeMsg);
+        this.query = query;
+        this.numVersion = numVersion;
+        this.confirmationUDPPort = confirmationUDPPort;
+    }
 
     public Query getQuery() {
         return query;
@@ -20,5 +36,13 @@ public class Prepare extends MulticastMSG{
 
     public void setNumVersion(int numVersion) {
         this.numVersion = numVersion;
+    }
+
+    public int getConfirmationUDPPort() {
+        return this.confirmationUDPPort;
+    }
+
+    public void setConfirmationUDPSocket(int confirmationUDPport) {
+        this.confirmationUDPPort = confirmationUDPPort;
     }
 }
