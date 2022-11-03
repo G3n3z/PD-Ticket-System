2615b2cff4f59cb4bd09ad9297d95c770657b365
jorhug <102927982+jorhug@users.noreply.github.com>
2022-11-03 16:06:26 +0000

    register user query

--- src/main/java/com/isec/pd22/utils/DBCommunicationManager.java
+++ src/main/java/com/isec/pd22/utils/DBCommunicationManager.java
@@ -2,24 +2,29 @@ package com.isec.pd22.utils;
 
 import com.isec.pd22.payload.ClientMSG;
 import com.isec.pd22.server.models.InternalInfo;
+import com.isec.pd22.server.models.Query;
 
-import java.io.ObjectOutputStream;
 import java.sql.*;
+import java.util.Date;
 
 public class DBCommunicationManager {
     private Connection connection;
     private ClientMSG clientMsg;
+    private InternalInfo internalInfo;
 
     public DBCommunicationManager(ClientMSG clientMsg, InternalInfo internalInfo) throws SQLException {
         this.clientMsg = clientMsg;
+        this.internalInfo = internalInfo;
         this.connection = DriverManager.getConnection(internalInfo.getUrl_db());
     }
 
-    public void registerUser(ClientMSG msgClient, ObjectOutputStream oos) {
-        //TODO
+    public void executeRegisterUser(Query query) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query.getQuery());
+        statement.close();
     }
 
-    public void loginUser(ClientMSG msgClient, ObjectOutputStream oos) throws SQLException {
+    public void loginUser(ClientMSG msgClient) throws SQLException {
         Statement stm = connection.createStatement();
         String query = "SELECT * FROM utilizador WHERE nome like '%" +
                 msgClient.getUsername() + "%' AND password like " + msgClient.getPassword();
@@ -34,5 +39,16 @@ public class DBCommunicationManager {
 
         }
 
+    }
+
+    public Query getRegisterUserQuery(ClientMSG msgClient) {
+        Query q;
+        String query = "INSERT INTO utilizador VALUES (NULL, " + msgClient.getUsername() + "," + msgClient.getNome() + "," + msgClient.getPassword() +", NULL, NULL)";
+        Date time = new Date();
+        long unixTime = time.getTime()/1000;
+        synchronized (internalInfo) {
+            q = new Query(internalInfo.getNumDB(), query, unixTime);
+        }
+        return q;
     }
 }
