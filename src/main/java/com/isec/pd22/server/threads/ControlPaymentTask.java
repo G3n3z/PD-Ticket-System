553eaf66b777fca527665b706a4e8faa1a81247f
jorhug <102927982+jorhug@users.noreply.github.com>
2022-11-13 16:34:24 +0000

    bug that made thread get stuck on prepared fixed on ControlPaymentTask

--- src/main/java/com/isec/pd22/server/threads/ControlPaymentTask.java
+++ src/main/java/com/isec/pd22/server/threads/ControlPaymentTask.java
@@ -45,14 +45,23 @@ public class ControlPaymentTask extends TimerTask {
     @Override
     public void run() {
         dbVM = new DBVersionManager(connection);
-        dbComm = new DBCommunicationManager(internalInfo,connection);
-        while(true) {
+        dbComm = new DBCommunicationManager(internalInfo, connection);
+        while (true) {
             System.out.println(dbComm.canCancelReservation(reservationId));
             if (dbComm.canCancelReservation(reservationId)) {
                 Query query = dbComm.deleteReservaNotPayed(reservationId);
                 if (startUpdateRoutine(query, internalInfo)) {
-                    break;
-                } else{
+                    try {
+                        dbVM.insertQuery(query);
+                        sendCommit();
+                    } catch (SQLException e) {
+                        System.out.println("[ControlPayment] - error on updateRoutine - could not send prepare: " + e.getMessage());
+                    } catch (IOException e) {
+                        System.out.println("[ControlPayment] - error on updateRoutine - could not send prepare: " + e.getMessage());
+                    }finally {
+                        break;
+                    }
+                } else {
                     try {
                         System.out.println("[ControlPaymentTask] - servers busy, trying again in a second...");
                         Thread.sleep(1000);
@@ -60,7 +69,7 @@ public class ControlPaymentTask extends TimerTask {
                         break;
                     }
                 }
-            }else {
+            } else {
                 break;
             }
         }
@@ -68,116 +77,111 @@ public class ControlPaymentTask extends TimerTask {
         timer.cancel();
     }
 
-    private boolean startUpdateRoutine(Query query, InternalInfo internalInfo) {
-        boolean repeat = true;
-        int confirmationCounter = 1;
+    private boolean startUpdateRoutine(Query query, InternalInfo internalInfo){
         Set<Prepare> confirmationList = new HashSet<>();
+        byte[] bytes = new byte[10000];
         //Mudar estado para UPDATE
-        synchronized (internalInfo){
+        synchronized (internalInfo) {
             internalInfo.setStatus(Status.UPDATING);
         }
         //Abrir socket UPD auto para confirmações
         try {
-            DatagramSocket confirmationSocket = new DatagramSocket(0);
-
-            //Enviar PREPARE por MC
-            byte[] bytes = new byte[10000];
             DatagramPacket dp = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(Constants.MULTICAST_IP), Constants.MULTICAST_PORT);
-            while (repeat) {
-                Prepare prepareMsg = new Prepare(
-                        TypeOfMulticastMsg.PREPARE,
-                        query,
-                        query.getNumVersion(),
-                        internalInfo.getIp(),
-                        confirmationSocket.getLocalPort()
-                );
+            DatagramSocket confirmationSocket = new DatagramSocket(0);
+            sendPrepare(dp, query, confirmationList, confirmationSocket);
 
-                ObjectStream objectStream = new ObjectStream();
-                objectStream.writeObject(dp, prepareMsg);
-
-                //Se não estiverem todas, reenviar PREPARE por MC
-                internalInfo.getMulticastSocket().send(dp);
-                try {
-                    //Esperar confirmações com timeout 1 sec
-                    confirmationSocket.setSoTimeout(1000);
-                    while (true) {
-                        confirmationSocket.receive(dp);
-                        Prepare confirmation = objectStream.readObject(dp, Prepare.class);
-                        confirmationList.add(confirmation);
-                    }
-                } catch (SocketException | SocketTimeoutException e) {
-                    //Verificar confirmações com lista de servidores
-                    if(confirmationCounter == 0){
-                        break;
-                    }else if(verifyConfirmations(confirmationList))
-                        repeat = false;
-                    else {
-                        confirmationList.clear();
-                        confirmationCounter--;
-                    }
-                }
-            }
-
+            return verifyConfirmations(confirmationList);
             //Verificar confirmações com lista de servidores
-            if(verifyConfirmations(confirmationList)){
-                //Se estiverem todos enviar COMMIT
-                try {
-                    dbVM.insertQuery(query);
-                    sendCommit(dp);
-                } catch (IOException | SQLException e) {
-                    System.out.println("[ControlPaymentTask] - error on updateRoutine - could not send commit: "+ e.getMessage());
-                    return false;
-                }
-                //Executar alteração
-                return true;
-            }
-            //Se não estiverem todas enviar ABORT
-            sendAbort(dp);
-            //Cancelar a operação e informar utilizador
-
-        }  catch (IOException e) {
-            System.out.println("[ControlPaymentTask] - error on updateRoutine - could not send prepare: "+ e.getMessage());
+        } catch (IOException e) {
+            System.out.println("[ControlPayment] - error on updateRoutine - could not send prepare: " + e.getMessage());
+            return false;
         }
-        return false;
     }
+
+    private void sendPrepare(DatagramPacket dp, Query query, Set<Prepare> confirmationList, DatagramSocket confirmationSocket) throws IOException {
+        //Enviar PREPARE por MC
+        boolean repeat = true;
+        int confirmationCounter = 1;
+
+        while (repeat) {
+            Prepare prepareMsg = new Prepare(
+                    TypeOfMulticastMsg.PREPARE,
+                    query,
+                    query.getNumVersion(),
+                    internalInfo.getIp(),
+                    confirmationSocket.getLocalPort()
+            );
+
+            ObjectStream objectStream = new ObjectStream();
+            objectStream.writeObject(dp, prepareMsg);
+
+            //Se não estiverem todas, reenviar PREPARE por MC
+            internalInfo.getMulticastSocket().send(dp);
+            try {
+                //Esperar confirmações com timeout 1 sec
+                confirmationSocket.setSoTimeout(1000);
+                while (true) {
+                    confirmationSocket.receive(dp);
+                    Prepare confirmation = objectStream.readObject(dp, Prepare.class);
+                    confirmationList.add(confirmation);
+                    if (confirmationList.size() == internalInfo.getHeatBeats().size() && verifyConfirmations(confirmationList)) {
+                        repeat = false;
+                        break;
+                    }
+                }
+            } catch (SocketException | SocketTimeoutException e) {
+                //Verificar confirmações com lista de servidores
+                if (confirmationCounter == 0) {
+                    break;
+                } else if (verifyConfirmations(confirmationList))
+                    repeat = false;
+                else {
+                    confirmationList.clear();
+                    confirmationCounter--;
+                }
+            }
+        }
+    }
+
     /**
      * Method to send an Abort message through Multicast
-     * @param dp Datagrampacket created for multicast message transmition
      * @throws IOException when it fails sending the packet
      */
-    private void sendAbort(DatagramPacket dp) throws IOException {
+    private void sendAbort() throws IOException {
+        DatagramPacket dp = new DatagramPacket(new byte[1000], 1000, InetAddress.getByName(Constants.MULTICAST_IP), Constants.MULTICAST_PORT);
         Abort abortMsg = new Abort(TypeOfMulticastMsg.ABORT);
         ObjectStream objectStream = new ObjectStream();
-        objectStream.writeObject(dp,abortMsg);
+        objectStream.writeObject(dp, abortMsg);
         internalInfo.getMulticastSocket().send(dp);
     }
 
     /**
      * Method to send a Commit message through Multicast
-     * @param dp Datagrampacket created for multicast message transmition
      * @throws IOException when it fails sending the packet
      */
-    private void sendCommit(DatagramPacket dp) throws IOException {
+    private void sendCommit() throws IOException {
+        DatagramPacket dp = new DatagramPacket(new byte[1000], 1000, InetAddress.getByName(Constants.MULTICAST_IP), Constants.MULTICAST_PORT);
         Commit commitMsg = new Commit(TypeOfMulticastMsg.COMMIT, internalInfo.getIp(), internalInfo.getPortUdp());
         ObjectStream os = new ObjectStream();
-        os.writeObject(dp,commitMsg);
+        os.writeObject(dp, commitMsg);
         internalInfo.getMulticastSocket().send(dp);
     }
 
     private boolean verifyConfirmations(Set<Prepare> confirmationList) {
-        if(confirmationList.size() == internalInfo.getHeatBeats().size()) {
-            int count = 0;
-            for (Prepare p : confirmationList) {
-                for (HeartBeat hb : internalInfo.getHeatBeats())
-                    if (p.getIp().equals(hb.getIp()) && p.getPortUdpClients() == hb.getPortUdp()) {
-                        count++;
-                        break;
-                    }
+        synchronized (internalInfo.getHeatBeats()) {
+            if (confirmationList.size() == internalInfo.getHeatBeats().size()) {
+                int count = 0;
+                for (Prepare p : confirmationList) {
+                    for (HeartBeat hb : internalInfo.getHeatBeats())
+                        if (p.getIp().equals(hb.getIp()) && p.getPortUdpClients() == hb.getPortUdp()) {
+                            count++;
+                            break;
+                        }
+                }
+                if (count == internalInfo.getHeatBeats().size())
+                    return true;
             }
-            if (count == internalInfo.getHeatBeats().size())
-                return true;
         }
         return false;
     }
-
 }
