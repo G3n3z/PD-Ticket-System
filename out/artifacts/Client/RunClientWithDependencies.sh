a4ddcb924970018f14041659f1d7445f82dbdeb0
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-12-01 15:18:36 +0000

    remove artifacts

--- out/artifacts/Client/RunClientWithDependencies.sh
+++
@@ -1,7 +0,0 @@
-# Note: Coloca-me na pasta onde está o ficheiro do client .jar
-
-java --module-path javafx-sdk-19/lib/ --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar PD-Ticket-System.jar
-
-## Old commands for historic
-# java --module-path /Users/diogo/Documents/Java_SDKs/javafx-sdk-19/lib/ --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar PD-Ticket-System.jar
-# javac --module-path /Users/diogo/Documents/Java_SDKs/javafx-sdk-19/lib/ --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -sourcepath ./src/ -d ./out/ ./src/main/java/com/isec/pd22/client/ui/MainJavaFX.java
